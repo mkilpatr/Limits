e48483885484f3d24766c0124eb7fe1f37fcb224
Matthew <mkilpatr@FNAL.GOV>
2021-03-03 18:51:58 -0600

    Switch between methods

--- Datacards/python/mergeJSONFiles.py
+++ Datacards/python/mergeJSONFiles.py
@@ -1,9 +1,17 @@
 import os
 import json
+import argparse
+
+parser = argparse.ArgumentParser(
+        description='Produce or print limits based on existing datacards')
+parser.add_argument("-r", "--region", dest="region", default='',
+                         help="Search region to use when running the maximum likelihood fit. [Default: Search region with 183]")
+args = parser.parse_args()
 
 # json file witll_BkgPred_v2e bkg predictions and signal yields
-#json_binMaps = 'Datacards/setup/SUSYNano19/dc_BkgPred_BinMaps_master.json'
-json_binMaps = 'Datacards/setup/SUSYNano19/dc_BkgPred_BinMaps_SSR_master.json'
+json_binMaps = ''
+if args.region == 'SSR': json_binMaps = 'Datacards/setup/SUSYNano19/dc_BkgPred_BinMaps_SSR_master.json'
+else:                    json_binMaps = 'Datacards/setup/SUSYNano19/dc_BkgPred_BinMaps_master.json'
 json_lepPred = 'Datacards/setup/SUSYNano19/ll_BkgPred.json'
 json_TTZPred = 'Datacards/setup/SUSYNano19/TTZ_pred.json'
 json_RarePred = 'Datacards/setup/SUSYNano19/Rare_pred.json'
